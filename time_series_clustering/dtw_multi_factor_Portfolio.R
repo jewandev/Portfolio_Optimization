af48c76eb459fe489a546eefca825cb5dd4cf0b2
이제완 <jewan.quant@gmail.com>
2021-05-07 09:47:11 +0900

    Update DTW Clustering Code

--- time_series_clustering/dtw_multi_factor_Portfolio.R
+++ time_series_clustering/dtw_multi_factor_Portfolio.R
@@ -12,4 +12,9 @@ krw = read.csv('data/krw_close.csv', row.names = 1,
                stringsAsFactors = FALSE) %>% as.xts()
 glimpse(krw)
 krw
+(ksdq_next_period[[1]][[1]])
+as.numeric(gsub('-','', ksdq_next_period[[1]][1])) #>= 201103
+ksdq_next_period[[1]][1]
 
+parse_number(ksdq_next_period[[1]])
+ksdq_next_period[[1]][parse_number(ksdq_next_period[[1]]) >= 201903]
